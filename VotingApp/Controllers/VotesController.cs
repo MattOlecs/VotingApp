4a825f5cf0bd26c3757c294ef35dffef028744b5
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2023-11-06 20:38:28 +0100

    controllers

---
+++ VotingApp/Controllers/VotesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VotingApp.CQRS.Commands.AddVoteCommand;
+using VotingApp.Infrastructure.CQRS.Interfaces;
+
+namespace VotingApp.Controllers;
+
+public class VotesController : AbstractController
+{
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public VotesController(ICommandDispatcher commandDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+    }
+    
+    [HttpPost("voting")]
+    public async Task<Guid> AddNewVoting()
+    {
+        return await _commandDispatcher.Dispatch<AddVoteCommand, Guid>(new AddVoteCommand());
+    }
+}
