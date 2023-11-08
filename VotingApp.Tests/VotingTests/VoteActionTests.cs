0b6b75949572e52be11296197d7390635cd69bb0
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2023-11-08 17:04:57 +0100

    exceptions tests

--- VotingApp.Tests/VotingTests/VoteActionTests.cs
+++ VotingApp.Tests/VotingTests/VoteActionTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using VotingApp.Infrastructure.Exceptions;
 using VotingApp.Records;
 using VotingApp.Services;
 using VotingApp.Services.Interfaces;
@@ -45,6 +46,36 @@ public class VoteActionTests
         var candidate = _votesService.GetCandidates()[candidateId];
         voter.HasVoted.Should().BeTrue();
         candidate.Votes.Should().Be(1);
+    }
+
+    [Fact]
+    public void voting_with_invalid_voter_id_throws_error()
+    {
+        //Given
+        _votesService.AddVoter(new Voter("Jan Kowalski"));
+        var correctCandidateId = _votesService.AddCandidate(new Candidate("Keanu Reeves"));
+        var incorrectVoterId = Guid.NewGuid();
+
+        //When
+        Action act = () => Act(incorrectVoterId, correctCandidateId);
+
+        //Then
+        act.Should().Throw<NotFoundException>();
+    }
+    
+    [Fact]
+    public void voting_with_invalid_candidate_id_throws_error()
+    {
+        //Given
+        _votesService.AddCandidate(new Candidate("Keanu Reeves"));
+        var correctVoterId = _votesService.AddVoter(new Voter("Jan Kowalski"));
+        var incorrectCandidateId = Guid.NewGuid();
+
+        //When
+        Action act = () => Act(correctVoterId, incorrectCandidateId);
+
+        //Then
+        act.Should().Throw<NotFoundException>();
     }
     
     #region Arrange
